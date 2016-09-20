3d03babbfa7d03b00315a6dec652085e476db4ad
David <david@interaction.cr>
2016-09-19 19:05:43 -0600

    adding build and production tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,143 +1,35 @@
 import gulp from 'gulp';
-import autoprefixer from 'autoprefixer';
-import browserify from 'browserify';
-import watchify from 'watchify';
-import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer';
-import eslint from 'gulp-eslint';
-import babelify from 'babelify';
-import uglify from 'gulp-uglify';
-import rimraf from 'rimraf';
-import notify from 'gulp-notify';
-import browserSync, { reload } from 'browser-sync';
-import sourcemaps from 'gulp-sourcemaps';
-import postcss from 'gulp-postcss';
 import rename from 'gulp-rename';
-import nested from 'postcss-nested';
-import vars from 'postcss-simple-vars';
-import extend from 'postcss-simple-extend';
-import cssnano from 'cssnano';
-import htmlReplace from 'gulp-html-replace';
-import imagemin from 'gulp-imagemin';
-import pngquant from 'imagemin-pngquant';
-import runSequence from 'run-sequence';
-import ghPages from 'gulp-gh-pages';
+import awspublish from 'gulp-awspublish';
 
-const paths = {
-  bundle: 'app.js',
-  entry: 'src/index.js',
-  srcCss: 'src/**/*.scss',
-  srcImg: 'src/images/**',
-  srcLint: ['src/**/*.js'],
-  dist: 'dist',
-  distJs: 'dist/js',
-  distImg: 'dist/images',
-  distDeploy: './dist/**/*',
+const config = {
+  pkg: require('./package.json'),
+  aws: require('./aws-keys.json'),
 };
 
-const customOpts = {
-  entries: [paths.entry],
-  debug: true,
-  cache: {},
-  packageCache: {},
-};
-
-const opts = Object.assign({}, watchify.args, customOpts);
-
-gulp.task('clean', cb => {
-  rimraf('dist', cb);
-});
-
-gulp.task('browserSync', () => {
-  browserSync({
-    server: {
-      baseDir: './',
+// AWS publisher
+gulp.task('publish', (cb) => {
+  const publisher = awspublish.create({
+    params: {
+      Bucket: config.pkg.bucket,
+      Region: config.pkg.bucketRegion,
     },
+    accessKeyId: config.aws.accessKeyId,
+    secretAccessKey: config.aws.secretAccessKey,
   });
-});
 
-gulp.task('watchify', () => {
-  const bundler = watchify(browserify(opts));
+  const headers = {
+    CacheControl: 'max-age=630720000, public',
+    Expires: new Date(Date.now() + 63072000000),
+  };
 
-  function rebundle() {
-    return bundler.bundle()
-      .on('error', notify.onError())
-      .pipe(source(paths.bundle))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(paths.distJs))
-      .pipe(reload({ stream: true }));
-  }
-
-  bundler.transform(babelify)
-  .on('update', rebundle);
-  return rebundle();
-});
-
-gulp.task('browserify', () => {
-  browserify(paths.entry, { debug: true })
-  .transform(babelify)
-  .bundle()
-  .pipe(source(paths.bundle))
-  .pipe(buffer())
-  .pipe(sourcemaps.init({ loadMaps: true }))
-  .pipe(uglify())
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest(paths.distJs));
-});
-
-gulp.task('styles', () => {
-  gulp.src(paths.srcCss)
-  .pipe(rename({ extname: '.css' }))
-  .pipe(sourcemaps.init())
-  .pipe(postcss([vars, extend, nested, autoprefixer, cssnano]))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest(paths.dist))
-  .pipe(reload({ stream: true }));
-});
-
-gulp.task('htmlReplace', () => {
-  gulp.src('index.html')
-  .pipe(htmlReplace({ css: 'styles/main.css', js: 'js/app.js' }))
-  .pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('images', () => {
-  gulp.src(paths.srcImg)
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngquant()],
+  gulp.src('./dist/**/*')
+    .pipe(rename((path) => {
+      path.dirname = `${config.pkg.bucketPath}/${path.dirname}`;
     }))
-    .pipe(gulp.dest(paths.distImg));
-});
-
-gulp.task('lint', () => {
-  gulp.src(paths.srcLint)
-  .pipe(eslint())
-  .pipe(eslint.format());
-});
-
-gulp.task('watchTask', () => {
-  gulp.watch(paths.srcCss, ['styles']);
-  gulp.watch(paths.srcLint, ['lint']);
-});
-
-gulp.task('deploy', () => {
-  gulp.src(paths.distDeploy)
-    .pipe(ghPages());
-});
-
-gulp.task('watch', cb => {
-  runSequence('clean', ['browserify', 'watchTask', 'watchify', 'styles', 'lint', 'images'], cb);
-});
-
-gulp.task('build', cb => {
-  process.env.NODE_ENV = 'production';
-  runSequence('clean', [
-    'browserify',
-    'styles',
-    // 'htmlReplace',
-    'images',], cb);
+    .pipe(awspublish.gzip())
+    .pipe(publisher.publish(headers))
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter())
+    .on('end', cb);
 });
---
+++ webpack.config.prod.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+const cssnext = require('postcss-cssnext');
+const postcssFocus = require('postcss-focus');
+const postcssReporter = require('postcss-reporter');
+const cssnano = require('cssnano');
+
+module.exports = {
+  devtool: 'hidden-source-map',
+
+  entry: {
+    app: [
+      './src/index.js',
+    ],
+    vendor: [
+      'react',
+      'react-dom',
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[chunkhash].js',
+    publicPath: '/',
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss'],
+    modules: [
+      'src',
+      'node_modules',
+    ],
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[hash:base64]&modules&importLoaders=1!postcss-loader'),
+      }, {
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass'),
+      }, {
+        test: /\.jsx*$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      }, {
+        test: /\.(jpe?g|gif|png|svg)$/i,
+        loader: 'url-loader?limit=10000',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.js',
+    }),
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new ManifestPlugin({
+      basePath: '/',
+    }),
+    new ChunkManifestPlugin({
+      filename: 'chunk-manifest.json',
+      manifestVariable: 'webpackManifest',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ],
+
+  postcss: () => [
+    postcssFocus(),
+    cssnext({
+      browsers: ['last 2 versions', 'IE > 10'],
+    }),
+    cssnano({
+      autoprefixer: false,
+    }),
+    postcssReporter({
+      clearMessages: true,
+    }),
+  ],
+};
